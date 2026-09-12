a785cc38be5d2914c52276526e58207581067847
Your Name <your_email@example.com>
2026-09-12 17:24:36 +0800

    Enter Stock Quanitity, Untill User Quits

--- auditor.py
+++ auditor.py
@@ -1,5 +1,11 @@
+stock_total = 0
+failed_entries = 0
 
+while True:
+    stock_count =input("Enter stock quanitity (type quit to stop): ")
 
+    if stock_count.lower() == "quit":
+        break
 
 
 
